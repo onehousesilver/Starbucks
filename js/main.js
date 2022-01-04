efb504a51ecb0602dfd50e9ef7eacb2f303aa64e
onehousesilver <onehousesilver@gmail.com>
2022-01-04 23:44:08 +0900

    0102

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 // document == HTML 자체로 이해해도 무방함
 
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
+/**
+ * 검색창 제어
+ */
+ // 검색창 요소(.search) 찾기.
+ const searchEl = document.querySelector('.search')
+ const searchInputEl = searchEl.querySelector('input')
+ // 검색창 요소를 클릭하면 실행.
+ searchEl.addEventListener('click', function () {
+   searchInputEl.focus()
+ })
+ // 검색창 요소 내부 실제 input 요소에 포커스되면 실행.
+ searchInputEl.addEventListener('focus', function () {
+   searchEl.classList.add('focused')
+   searchInputEl.setAttribute('placeholder', '통합검색')
+ })
+ // 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
+ searchInputEl.addEventListener('blur', function () {
+   searchEl.classList.remove('focused')
+   searchInputEl.setAttribute('placeholder', '')
+ })
 
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-})
-
-searchInputEl.addEventListener('focus', function () {
-  searchInputEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchInputEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
+ // searchEl과 searchInputEl에서 정확한 요소로 타겟팅! 실수하지 않기

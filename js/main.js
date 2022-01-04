39831578ff57509057e20fae9d9de0ec199e0255
onehousesilver <onehousesilver@naver.com>
2022-01-05 01:11:01 +0900

    badge

--- js/main.js
+++ js/main.js
@@ -3,22 +3,47 @@
 /**
  * 검색창 제어
  */
- // 검색창 요소(.search) 찾기.
- const searchEl = document.querySelector('.search')
- const searchInputEl = searchEl.querySelector('input')
- // 검색창 요소를 클릭하면 실행.
- searchEl.addEventListener('click', function () {
-   searchInputEl.focus()
- })
- // 검색창 요소 내부 실제 input 요소에 포커스되면 실행.
- searchInputEl.addEventListener('focus', function () {
-   searchEl.classList.add('focused')
-   searchInputEl.setAttribute('placeholder', '통합검색')
- })
- // 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
- searchInputEl.addEventListener('blur', function () {
-   searchEl.classList.remove('focused')
-   searchInputEl.setAttribute('placeholder', '')
- })
+// 검색창 요소(.search) 찾기.
+const searchEl = document.querySelector('.search')
+const searchInputEl = searchEl.querySelector('input')
+// 검색창 요소를 클릭하면 실행.
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus()
+})
+// 검색창 요소 내부 실제 input 요소에 포커스되면 실행.
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused')
+  searchInputEl.setAttribute('placeholder', '통합검색')
+})
+// 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused')
+  searchInputEl.setAttribute('placeholder', '')
+})
 
- // searchEl과 searchInputEl에서 정확한 요소로 타겟팅! 실수하지 않기
+// searchEl과 searchInputEl에서 정확한 요소로 타겟팅! 실수하지 않기
+
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY)
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    // gsap.to(요소, 지속시간, 옵션)
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: 'none'
+    });
+
+  } else {
+    // 배지 보이기
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: 'block'
+    });
+
+  }
+}, 300));
+
+// _.throttle(함수, 시간)

bd46028879382ed333d1b5f7d736cb1258c233c6
onehousesilver <onehousesilver@naver.com>
2022-01-07 02:35:09 +0900

    youtube

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ window.addEventListener('scroll', _.throttle(function () {
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
   gsap.to(fadeEl, 1, {
-    delay: (index+1) * .7,
+    delay: (index + 1) * .7,
     opacity: 1,
   });
 });
@@ -93,3 +93,28 @@ promotionToggleBtn.addEventListener('click', function () {
     promotionEl.classList.remove('hide')
   }
 })
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+
+function floatingObject(selector, delay, size) {
+  gsap.to(
+    selector,
+    random(1.5, 2.5), 
+    {
+      y: size,
+      repeat: -1,
+      yoyo: true,
+      ease: Power1.easeInOut,
+      delay: random(0, delay)
+    })
+}
+
+floatingObject('.floating1', 1, 15)
+floatingObject('.floating2', .5, 15)
+floatingObject('.floating3', 1.5, 20)
